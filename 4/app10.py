e1a418a0b37141d21bcbe69eb0d23bc0046ab028
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-08-05 14:37:02 +0200

    changes

--- 4/app10.py
+++ 4/app10.py
@@ -2,7 +2,7 @@
 #Считалка
 start_number = int(input("Введите начало счета\n"))
 finish_number = int(input("Введите конец счета\n"))
-interval = int(input("Введите интервал счета\n\n"))
+interval = int(input("Введите интервал счета\n"))
 for i in range(start_number, finish_number, interval):
     print(i)
 input("\n\nНажмите Enter, чтобы выйти")
