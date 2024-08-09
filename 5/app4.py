03de26292f9116ec94ddeae300410797b5d8a8c4
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-08-09 13:28:56 +0200

    added

---
+++ 5/app4.py
@@ -0,0 +1,4 @@
+#Демонстрирует функцию len(), чтобы определить количество элементов  в списке  
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+input("\n\nНажмите Enter, чтобы выйти")
