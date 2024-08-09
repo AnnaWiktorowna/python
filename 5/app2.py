38950c16d0ddf057c951ff11b33ab9ce2f5e5bc2
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-08-09 13:13:21 +0200

    added

--- 5/app2.py
+++ 5/app2.py
@@ -1,4 +1,5 @@
-#Демонстрирует создание списка
+#Демонстрирует изменение значения элемента
 thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
 print(thislist)
 input("\n\nНажмите Enter, чтобы выйти")
