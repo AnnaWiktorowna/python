2fb892e6cfb359e7cbd440363668db7323d302a0
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-08-26 14:50:59 +0200

    added

---
+++ 5/app8.py
@@ -0,0 +1,5 @@
+#Демонстрирует удаление элемента в списке с помощью метода remove(). 
+thislist = ["apple", "banana", "cherry"] #список
+thislist.remove("banana") #удаление элемента из списка
+print(thislist)
+input("\n\nНажмите Enter, чтобы выйти")
