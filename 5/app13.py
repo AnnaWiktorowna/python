24d7a2c39cabfe56a27a499cd34e7a281f394131
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-08-30 12:53:27 +0200

    added

---
+++ 5/app13.py
@@ -0,0 +1,7 @@
+#Цыклические списки for
+#Перебор индексных номеров с помощью функций range() и len()
+#распечатать все элементы, указав их порядковый номер
+thislist = ["apple", "banana", "cherry"] #список
+for i in range(len(thislist)):
+    print(thislist[i])
+input("\n\nНажмите Enter, чтобы выйти")
