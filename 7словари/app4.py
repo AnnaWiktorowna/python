7554c076fdea610851e312b8b8bfc3d2a3a64bd0
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-06 16:33:36 +0200

    added

---
+++ 7словари/app4.py
@@ -0,0 +1,10 @@
+#Словари
+#типы данных
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "electric" : False,
+    "year" : 1964,
+    "colors" : ["red", "white", "blue"]
+}
+print(thisdict)
