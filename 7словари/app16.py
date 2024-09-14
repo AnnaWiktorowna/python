98a47659dabc210c25ead7b59620466f0ec0fbec
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-14 16:34:45 +0200

    added

---
+++ 7словари/app16.py
@@ -0,0 +1,9 @@
+#Словари
+#Проверить, есть ли слово "model" в словаре
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+if "model" in thisdict:
+    print("Да, 'model' - один из ключей в словаре thisdict")
