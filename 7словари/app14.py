fcae3c375ddd642e358f512de21194b31cb1386f
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-14 16:16:32 +0200

    added

---
+++ 7словари/app14.py
@@ -0,0 +1,9 @@
+#Словари
+#Получить спмсок пар ключ : значение с помощью метода  items()
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+x = thisdict.items()
+print(x)
