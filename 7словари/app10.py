8c478a56f17bfd609ce8a7c9d4cd2484c2f7197e
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-14 15:45:48 +0200

    added

---
+++ 7словари/app10.py
@@ -0,0 +1,11 @@
+#Словари
+#Добавление нового элемента в исходный словарь
+car = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+x = car.keys()
+print(x)
+car["color"] = "white"
+print(x)
