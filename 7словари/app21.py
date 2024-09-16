81d94cebc2f5f6c37462e93a8ed58a341a8ad79c
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-16 12:38:06 +0200

    added

---
+++ 7словари/app21.py
@@ -0,0 +1,9 @@
+#Словари
+#удаление элемента из словаря с помощью метода pop()
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+thisdict.pop("model")
+print(thisdict)
