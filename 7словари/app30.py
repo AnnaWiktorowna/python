570889f96b4de6a499e7b1e48ef0e16ee57573fb
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-16 13:30:26 +0200

    added

---
+++ 7словари/app30.py
@@ -0,0 +1,9 @@
+#Словари
+#циклы по словарю.Использование метода items() для возврата ключей и значений словаря
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+for x, y in thisdict.items():
+    print(x, y)
