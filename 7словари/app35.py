a54c6b7930b97067156f39070d36a70f0b543ee7
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-16 14:17:08 +0200

    added

---
+++ 7словари/app35.py
@@ -0,0 +1,18 @@
+#Словари
+#Вложенные словари. Доступ к элементам. Вывести имя ребенка2
+myfamily = {
+    "child1" : {
+        "name" : "Emil",
+        "year" : 2004
+    },
+    "child2" : {
+        "name" : "Tobias",
+        "year" : 2007
+    },
+    "child3" : {
+        "name" : "Linus",
+        "year" : 2011
+    }
+}
+print(myfamily["child1"]["name"])
+
