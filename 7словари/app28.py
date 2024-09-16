b1943828fa28a33776baddf6ae7b84a81baf9066
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-16 13:22:28 +0200

    added

---
+++ 7словари/app28.py
@@ -0,0 +1,9 @@
+#Словари
+#циклы по словарю.Использование метода values() для возврата значений словаря
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+for x in thisdict.values():
+    print(x)
---
+++ 7словари/app29.py
@@ -0,0 +1,9 @@
+#Словари
+#циклы по словарю.Использование метода keys() для возврата ключей словаря
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+for x in thisdict.keys():
+    print(x)
