5265ce55471e0db3ca1fd5ed18e23545fd41a2ab
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-24 20:47:33 +0200

    added

---
+++ 8Кортежи/app5.py
@@ -0,0 +1,4 @@
+#Кортежи
+#Создать кортеж с помощью метода tuple()
+thistuple = tuple(("apple", "banana", "cherry"))
+print(thistuple)
---
+++ 8Кортежи/app6.py
@@ -0,0 +1,5 @@
+#Кортежи
+#Доступ к элементам
+#Выведите второй элемент кортежа
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
