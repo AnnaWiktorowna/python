03002461420bf72b668edf75b1423a7f2ca5b46b
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-09-29 13:00:35 +0200

    added

---
+++ 8Кортежи/app10.py
@@ -0,0 +1,5 @@
+#Кортежи
+#Диапазон индексов
+#В этом примере возвращаются элементы од cherry и до конца
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
