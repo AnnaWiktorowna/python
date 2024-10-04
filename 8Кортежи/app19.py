ffdbe9750fb2747ca8040995e144ab79a4fb9f57
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-10-04 11:40:50 +0200

    added

---
+++ 8Кортежи/app19.py
@@ -0,0 +1,7 @@
+#Кортежи
+#Распаковка кортежа. Использование Астерикс* в средине
+thistuple = ("apple", "mango", "papaya", "pineapple", "cherry")
+(green, *tropic, red) = thistuple
+print(green)
+print(tropic)
+print(red)
