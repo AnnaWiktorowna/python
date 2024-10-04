e6705d63b8e0ccac909c934f9a3f16666c92449f
AnnaWiktorowna <117044893+AnnaWiktorowna@users.noreply.github.com>
2024-10-04 13:01:59 +0200

    added

---
+++ 8Кортежи/app22.py
@@ -0,0 +1,7 @@
+#Кортежи
+#Цикл по кортежу с перебором индексных номеров используя цикл while
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+    print(thistuple[i])
+    i = i + 1
